ca15012b6c0c64e797e9e80fb822c3a459f9f12d
theRed-Coder <theRedCoder@tensorplex.ai>
2024-05-01 20:24:49 +0630

    hooks for api integration

---
+++ hooks/useWorkerLoginAuth.ts
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+interface LoginAuthResponse {
+  success: boolean;
+  body?: {
+    token: string;
+  };
+  error?: string;
+}
+
+interface LoginAuthPayload {
+  walletAddress: string;
+  chainId: string;
+  signature: string;
+  message: string;
+  timestamp: number;
+}
+
+const useWorkerLoginAuth = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const workerLoginAuth = async (payload: LoginAuthPayload) => {
+    setLoading(true);
+    try {
+      const endpoint = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/worker/login/auth`;
+      
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+
+        },
+        body: JSON.stringify(payload),
+      });
+
+      const data: LoginAuthResponse = await response.json();
+
+      if (response.ok && data.success) {
+        if (data.body?.token) {
+            localStorage.setItem('token', data.body.token);
+          } else {
+            throw new Error('Token is undefined');
+          }
+        } else {
+        throw new Error(data.error || `HTTP error! status: ${response.status}`);
+      }
+    } catch (e: any) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { workerLoginAuth, loading, error };
+};
+
+export default useWorkerLoginAuth;
