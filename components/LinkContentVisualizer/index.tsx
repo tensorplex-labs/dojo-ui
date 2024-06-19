00cb9424eeff8f5350e1b1c0ebadd399ff8fc3b1
jarvis8x7b <jarvis@tensorplex.ai>
2024-06-20 01:43:53 +0800

    fix: use codesandbox embed url

--- components/LinkContentVisualizer/index.tsx
+++ components/LinkContentVisualizer/index.tsx
@@ -34,19 +34,45 @@ const LinkContentVisualizer: React.FC<LinkContentVisualizerProps> = ({
     },
     [onRatingChange]
   );
+
+  const parseSandboxURL = (url: string) => {
+    const urlObj = new URL(url);
+    const sandboxId = urlObj.hostname.split('.')[0];
+    return `https://codesandbox.io/embed/${sandboxId}?view=preview&hidedevtools=1&hidenavigation=1&previewwindow=browser`;
+  };
+
   return (
     <div className="flex size-full flex-col justify-center ">
       {showTitle && <p className={`text-start font-bold ${FontSpaceMono.className}`}>{title}</p>}
+
       <div
         className={`h-auto w-full rounded-none ${showSlider && 'border-2 border-black bg-[#F6F6E6] shadow-brut-sm'} `}
       >
-        <iframe
-          src={url}
-          className="aspect-[3/4] w-full"
-          title="elastic-newton-69zqqk"
-          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-        />
+        <div
+          style={{
+            position: 'relative',
+            width: '100%',
+            height: '650px',
+            overflow: 'hidden',
+          }}
+        >
+          <iframe
+            src={parseSandboxURL(url)}
+            style={{
+              position: 'absolute',
+              left: '-16px',
+              bottom: '0px',
+              top: '0',
+              width: 'calc(100% + 16px)',
+              height: 'calc(100%)',
+              border: 'none',
+            }}
+            className="aspect-[3/4] w-full"
+            title="elastic-newton-69zqqk"
+            allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+            sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+          />
+        </div>
         {showSlider && (
           <>
             <div
