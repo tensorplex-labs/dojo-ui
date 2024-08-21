f5f5dccdcd2b48ea8609afb5b51cffb3aed460f9
theRed-Coder <theRedCoder@tensorplex.ai>
2024-08-21 16:05:27 +0700

    fixed navigation

--- components/Common/NavigationBar/index.tsx
+++ components/Common/NavigationBar/index.tsx
@@ -20,43 +20,41 @@ const NavigationBar = ({ openModal, isHomePage, className }: NavigationBarProps)
     <div className="flex items-center justify-center py-4">
       <nav
         className={cn(
-          'bg-background border-[1px] border-black rounded-md flex px-[20px] py-[12px] w-[1075px]',
+          'w-[1075px] h-[67.6px] bg-background border-[1px] border-black rounded-md flex px-[20px] py-[12px] items-center',
           className
         )}
       >
-        <div className=" w-full">
-          <div className="flex justify-between">
-            <div className="flex items-center gap-[30px]">
-              <div>
-                <Link href="/">
-                  <img className="h-[20px] w-[220px] md:h-[30px]" src="/logo.svg" alt="logo" />
+        <div className=" flex h-[45px] w-full justify-between ">
+          <div className="flex h-full items-center gap-[30px]">
+            <div className="">
+              <Link href="/">
+                <img className="h-[20px] w-[220px] md:h-[30px]" src="/logo.svg" alt="logo" />
+              </Link>
+            </div>
+            <div className="hidden items-center gap-[10px] md:flex">
+              {headerItems.map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.url}
+                  className={cn(
+                    'text-font-primary hover:cursor-pointer visited:text-font-primary hover:underline hover:text-primary'
+                  )}
+                >
+                  {item.title}
                 </Link>
-              </div>
-              <div className="hidden items-center gap-[10px] md:flex">
-                {headerItems.map((item, index) => (
-                  <Link
-                    key={index}
-                    href={item.url}
-                    className={cn(
-                      'text-font-primary hover:cursor-pointer visited:text-font-primary hover:underline hover:text-primary'
-                    )}
-                  >
-                    {item.title}
-                  </Link>
-                ))}
-              </div>
+              ))}
             </div>
-            <div className="flex items-center gap-3">
-              {!isHomePage ? (
-                <>
-                  <div className="flex items-center justify-center gap-[8px]">
-                    <WalletButton openModal={openModal}></WalletButton>
-                  </div>
-                </>
-              ) : (
-                <GetStartedButton />
-              )}
-            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            {!isHomePage ? (
+              <>
+                <div className="flex items-center justify-center gap-[8px]">
+                  <WalletButton openModal={openModal}></WalletButton>
+                </div>
+              </>
+            ) : (
+              <GetStartedButton />
+            )}
           </div>
         </div>
       </nav>
