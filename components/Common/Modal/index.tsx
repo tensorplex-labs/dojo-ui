d06653f1026befcd3bec7b747654ce568fb3faab
theRed-Coder <theRedCoder@tensorplex.ai>
2024-08-27 13:11:45 +0700

    fixed the Subscription Modal UI breaking issue

--- components/Common/Modal/index.tsx
+++ components/Common/Modal/index.tsx
@@ -10,8 +10,8 @@ function Modal({ title, showModal, setShowModal, btnText, className, children }:
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden text-black outline-none backdrop-blur-sm backdrop-brightness-50 focus:outline-none">
-      <div className="relative">
-        <div className="border-1 relative top-0 m-2 flex w-[900px] min-w-[300px] border-collapse flex-col border border-neutral-950 bg-card-background shadow-brut-sm outline-none focus:outline-none md:m-0">
+      <div className="relative px-4">
+        <div className="border-1 relative top-0 m-2 flex lg:w-[900px] min-w-[300px] border-collapse flex-col border border-neutral-950 bg-card-background shadow-brut-sm outline-none focus:outline-none md:m-0">
           <div className="flex items-start justify-between border border-solid border-gray-900">
             <div className="flex h-[46px] items-center pl-[22px]">
               <h1 className={`${FontSpaceMono.className} text-base font-bold`}>{title}</h1>
