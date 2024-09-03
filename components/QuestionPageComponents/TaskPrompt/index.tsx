df019735f47a3eb3bd7bfd49914869e7f7964014
v2img <v2img@tensorplex.ai>
2024-09-03 09:47:14 +0800

    changed p to div

--- components/QuestionPageComponents/TaskPrompt/index.tsx
+++ components/QuestionPageComponents/TaskPrompt/index.tsx
@@ -7,7 +7,7 @@ const TaskPrompt: React.FC<TaskPromptProps> = ({ title, taskType, formattedPromp
   const { updateQueryString } = useQueryFunc();
   return (
     <div className="flex max-w-[1200px] flex-col justify-center md:px-4 md:py-2 lg:px-4 lg:py-2">
-      <p className={`flex items-center text-start ${FontManrope.className} gap-2 text-2xl font-bold`}>
+      <div className={`flex items-center text-start ${FontManrope.className} gap-2 text-2xl font-bold`}>
         {title}
         {`  `}
         <div
@@ -15,7 +15,7 @@ const TaskPrompt: React.FC<TaskPromptProps> = ({ title, taskType, formattedPromp
         >
           {taskType} PROMPT
         </div>
-      </p>
+      </div>
       <div
         className={`${FontManrope.className}  my-5 flex self-start whitespace-pre-wrap rounded-xl border-2 border-black bg-ecru-white p-5 text-left text-base font-medium opacity-60`}
       >
