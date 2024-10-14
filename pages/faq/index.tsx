e733562395026dd3a9c3d034ebadfa9e89a41fba
v2img <v2img@tensorplex.ai>
2024-10-14 13:01:23 +0800

    added SEO changes

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -5,6 +5,7 @@ import { WalletManagement } from '@/components/TaskListPageComponents';
 import { useModal } from '@/hooks/useModal';
 import { MODAL } from '@/types/ProvidersTypes';
 import { FontSpaceMono } from '@/utils/typography';
+import Head from 'next/head';
 import { useState } from 'react';
 import { useAccount } from 'wagmi';
 
@@ -15,28 +16,37 @@ const Page = () => {
   const { address } = useAccount();
 
   return (
-    <div className="h-full bg-background text-black">
-      <div className="flex h-[320px] flex-col gap-4 border-b-2 border-black bg-background-accent">
-        <NavigationBar openModal={() => setShowUserCard(true)} />
-        <h1
-          className={`${FontSpaceMono.className} px-4 text-center text-4xl font-bold tracking-wide text-font-primary`}
-        >
-          FREQUENTLY ASKED QUESTIONS
-        </h1>
+    <>
+      <Head>
+        <title>Learn about Dojo Crowd Sourcing Platform for AI Model Development</title>
+        <meta
+          name="description"
+          content="Learn about Dojo and how to get started. Crowd Sourcing and support Open Source AI Models (Bittensor Subnet, etc)"
+        ></meta>
+      </Head>
+      <div className="h-full bg-background text-black">
+        <div className="flex h-[320px] flex-col gap-4 border-b-2 border-black bg-background-accent">
+          <NavigationBar openModal={() => setShowUserCard(true)} />
+          <h1
+            className={`${FontSpaceMono.className} px-4 text-center text-4xl font-bold tracking-wide text-font-primary`}
+          >
+            FREQUENTLY ASKED QUESTIONS
+          </h1>
+        </div>
+        <FAQHero />
+        <FAQList />
+        {showUserCard && (
+          <WalletManagement
+            address={address || ''}
+            openModal={openModal}
+            closeModal={setShowUserCard}
+            setShowUserCard={setShowUserCard}
+            setShowSubscriptionCard={setIsModalVisible}
+          />
+        )}{' '}
+        {isModalVisible && <SubscriptionModal setIsModalVisible={setIsModalVisible} isModalVisible={isModalVisible} />}
       </div>
-      <FAQHero />
-      <FAQList />
-      {showUserCard && (
-        <WalletManagement
-          address={address || ''}
-          openModal={openModal}
-          closeModal={setShowUserCard}
-          setShowUserCard={setShowUserCard}
-          setShowSubscriptionCard={setIsModalVisible}
-        />
-      )}{' '}
-      {isModalVisible && <SubscriptionModal setIsModalVisible={setIsModalVisible} isModalVisible={isModalVisible} />}
-    </div>
+    </>
   );
 };
 
